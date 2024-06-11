d35711c815216cf502718e7173fbf52fd281095b
anthony <angrimson@uni-hildesheim.de>
2024-06-11 13:04:06 +0200

    removing some local files.

--- local_loop_deepset_evaluate.sh
+++
@@ -1,7 +0,0 @@
-for ((counter=1; counter<=$1; counter++))
-do
-  echo "Starting " "$3" " Job" "$counter"/"$1"
-  python deepset_evaluate.py -i "$counter" --batch_size "$2" --regime "$3"
-  sleep 0.2
-done
-
